c0765a1a47e3ed9fca1d0fab905b3b54448a14c3
snusnusnu <kevin.helms@gmail.com>
2016-03-17 09:52:27 -0700

    display uploaded date

--- js/main.js
+++ js/main.js
@@ -1,25 +1,78 @@
+Vue.filter('number', function (value) {
+    var n = parseInt(value);
+    return n.toLocaleString('en');
+});
+
 new Vue({
     el: 'body',
     props: ['search', 'maxResults', 'list'],
     data: {
         search: '',
-        maxResults: 12,
-        list: []
+        maxResults: 48,
+        list: [],
+        listDetails: []
+    },
+    computed: {
+        currentYear: function () {
+            var d = new Date();
+            return d.getFullYear();
+        }
     },
     methods: {
-        fetchVideoList: function () {
+        buildRequestUrl: function (requestType, numVideos) {
             var apiKey = 'AIzaSyBc_Jr5X4lJXlSwXuBfA3u4cIx6MFTcuJA';
-            var requestUrl = 'https://www.googleapis.com/youtube/v3/search?part=snippet';
-            requestUrl += '&q=' + this.search.replace(' ', '+') + ' live';
-            requestUrl += '&maxResults=' + this.maxResults;
-            requestUrl += '&type=video&videoDefinition=high&videoDuration=long&videoEmbeddable=true&key=' + apiKey;
+            var requestUrl = 'https://www.googleapis.com/youtube/v3/';
+            var args = '';
+
+            switch (requestType) {
+                case 'search':
+                    args = '&q=' + this.search.replace(' ', '+') + '+live';
+                    args += '&maxResults=' + this.maxResults;
+                    args += '&type=video&videoDefinition=high&videoDuration=long&videoEmbeddable=true';
+                    break;
+
+                case 'videos':
+                    var videoIds = '';
+                    for (var i = 0; i < numVideos; i++) {
+                        if (i < numVideos - 1) {
+                            videoIds += this.list[i].id.videoId + '%2C';
+                        } else {
+                            videoIds += this.list[i].id.videoId;
+                        }
+                    }
+                    args = '%2Cstatistics&id=' + videoIds + '&fields=items(snippet%2Cstatistics)';
+                    break;
+            }
+
+            requestUrl += requestType + '?part=snippet';
+            requestUrl += args;
+            requestUrl += '&key=' + apiKey;
+            return requestUrl;
+        },
+        fetchVideoList: function () {
+            var requestType = 'search';
+            var requestUrl = this.buildRequestUrl(requestType);
 
             this.$http
                 .jsonp(requestUrl)
                 .then(function (response) {
                     this.list = response.data.items;
-                }, function (response) {
+                    var numVideos = response.data.items.length;
+                    this.fetchVideoDetails(numVideos);
+                }, function () {
                     return 'There was an error with the request.';
+                });
+        },
+        fetchVideoDetails: function (numVideos) {
+            var requestType = 'videos';
+            var requestUrl = this.buildRequestUrl(requestType, numVideos);
+
+            this.$http
+                .jsonp(requestUrl)
+                .then(function (response) {
+                    this.listDetails = response.data.items;
+                }, function () {
+                    return 'There was an error with the request.'
                 });
         },
         convertPublishedAt: function (publishedAt) {
@@ -31,6 +84,10 @@ new Vue({
             day = parseInt(day, 10);
             date = month + ' ' + day + ' ' + year;
             return date;
+        },
+        openLightbox: function (videoId) {
+            var lightbox = lity();
+            lightbox('//www.youtube.com/watch?v=' + videoId);
         }
     }
 });
